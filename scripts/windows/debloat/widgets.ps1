98eddb0c83bfce583ac66ba10bae207cee383ec8
GameSoul7Eugene <59319082+GameSoul7Eugene@users.noreply.github.com>
2026-08-27 17:10:55 +0200

    Update widgets.ps1

--- scripts/windows/debloat/widgets.ps1
+++ scripts/windows/debloat/widgets.ps1
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# winpodx debloat: Widgets / Taskbar news panel.
+# winpodx debloat: widgets / taskbar news panel
 
 Write-Host "[widgets] Disabling widgets / taskbar news panel..."
 
@@ -8,11 +8,15 @@ $widgetValues = @(
     @{Path="HKLM:\Software\Policies\Microsoft\Dsh"; Name="AllowNewsAndInterests"; Value=0},
     @{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name="TaskbarDa"; Value=0},
 
-    # Windows 10 -- taskbar News and interests feed
+    # Windows 10 -- news and interests feed on the taskbar
     @{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds"; Name="EnShellFeedsTaskbarViewMode"; Value=10750826},
     @{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds"; Name="ShellFeedsTaskbarPreviousViewMode"; Value=1},
     @{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds"; Name="ShellFeedsTaskbarContentUpdateMode"; Value=1},
-    @{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds"; Name="ShellFeedsTaskbarOpenOnHover"; Value=0}
+    @{Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds"; Name="ShellFeedsTaskbarOpenOnHover"; Value=0},
+	
+	# Misc
+	@{Path="HKLM:\Software\Policies\Microsoft\Windows\Windows Feeds"; Name="EnableFeeds"; Value=0},
+	@{Path="HKLM:\Software\Microsoft\PolicyManager\default\NewsAndInterests\AllowNewsAndInterests"; Name="value"; Value=0}
 )
 
 foreach ($item in $widgetValues) {
